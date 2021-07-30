cea336052c3616dbc72468898f3d7c4df33689fe
Bowen Wang <2529741W@student.gla.ac.uk>
2021-07-30 02:53:49 +0100

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -19,12 +19,10 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context_dict)
 
-def about(request): 
+def about(request):
+    # Spoiler: you don't need to pass a context dictionary here.
+    return render(request, 'rango/about.html')
 
-    about_message = {'boldmessage': 'This tutorial has been put together by Bowen Wang'}
-
-    return render(request, 'rango/about.html', context=about_message)
-    
 def show_category(request, category_name_slug):
     context_dict = {}
 
@@ -48,7 +46,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     
@@ -62,7 +60,7 @@ def add_page(request, category_name_slug):
     
     # You cannot add a page to a Category that does not exist... DM
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
